2b06d149c82b4552ff70cc4c5ac0a47bf429a80f
okumurameiya <105775374+okumurameiya@users.noreply.github.com>
2022-05-23 09:44:07 +0900

    show text in variable

--- training.js
+++ training.js
@@ -1,5 +1,13 @@
 
 // コンソールにHello worldを表示
+<<<<<<< Updated upstream
 
 let helloworld = "Hello world";
 console.log(helloworld);
+=======
+console.log("Hello world");
+
+// コンソールに変数内テキストを表示
+let yourname = "お名前";
+console.log("Hello %sさん", yourname);
+>>>>>>> Stashed changes
