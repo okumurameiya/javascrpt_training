5497a80104dfc73e77533199654adf4e4eeccb23
okumurameiya <105775374+okumurameiya@users.noreply.github.com>
2022-05-23 09:47:03 +0900

    Update training.js

--- training.js
+++ training.js
@@ -1,13 +1,8 @@
 
 // コンソールにHello worldを表示
-<<<<<<< Updated upstream
-
-let helloworld = "Hello world";
-console.log(helloworld);
-=======
 console.log("Hello world");
 
 // コンソールに変数内テキストを表示
 let yourname = "お名前";
 console.log("Hello %sさん", yourname);
->>>>>>> Stashed changes
+
